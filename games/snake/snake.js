07f460fd57c427b525a77d900ab1188917ce17ff
Austin Kurtti <austin.kurtti@gmail.com>
2016-04-30 22:33:54 -0500

    Added convenience features to snake.

--- games/snake/snake.js
+++ games/snake/snake.js
@@ -1,17 +1,20 @@
 // form variables
-var difficultyRadio;
+var difficultyRadio, highScore;
 
 // canvas variables
 var canvas, context, canvasHeight, canvasWidth;
 
 // game variables
 var cellWidth = 10,
+    gameStarted = false,
+    difficultySelected = false,
     gameLoop, direction, food, score, snakeArray;
 
 // give canvas variables values when ready
 $(document).ready(function() {
     $(document).foundation();
     
+    highScore = $("#high-score");
     difficultyRadio = $("input[name=difficulty]");
     difficultyRadio.on("change", setCanvasVars);
     
@@ -22,6 +25,8 @@ $(document).ready(function() {
 function setCanvasVars() {
     var difficulty = $("input[name=difficulty]:checked").val(),
         size;
+    
+    difficultySelected = true;
     
     switch(difficulty) {
         case "easy":
@@ -39,6 +44,10 @@ function setCanvasVars() {
         case "pro":
             size = 150;
         break;
+        
+        default:
+            difficultySelected = false;
+        break;
     }
     
     canvas.height = size;
@@ -46,12 +55,13 @@ function setCanvasVars() {
     canvasHeight = $("#snake-canvas").height();
     canvasWidth = $("#snake-canvas").width();
     
-    paintCanvas();
+    paintCanvas(true);
 }
 
 function init() {
     difficultyRadio.attr("disabled", true);
     
+    gameStarted = true;
     direction = "right";
     createSnake(5);
     createFood();
@@ -61,9 +71,14 @@ function init() {
 }
 
 function gameOver() {
-    difficultyRadio.attr("disabled", false);
-    
     clearInterval(gameLoop);
+    difficultyRadio.attr("disabled", false);
+    paintCanvas(true);
+    gameStarted = false;
+    
+    if(score > parseInt(highScore.text())) {
+        highScore.text(score);
+    }
 }
 
 function createSnake(length) {
@@ -80,15 +95,22 @@ function createFood() {
     };
 }
 
-function paintCanvas() {
+function paintCanvas(empty) {
     context.fillStyle = "white";
     context.fillRect(0, 0, canvasWidth, canvasHeight);
     context.strokeStyle = "black";
     context.strokeRect(0, 0, canvasWidth, canvasHeight);
+    if(empty && difficultySelected) {
+        var controlText = "Use WASD or Arrow keys",
+            emptyText = "Press SPACE to start!";
+        context.fillStyle = "black";
+        context.fillText(controlText, (canvasWidth/2)-58, (canvasHeight/2)-8);
+        context.fillText(emptyText, (canvasWidth/2)-50, (canvasHeight/2)+8);
+    }
 }
 
 function paint() {
-    paintCanvas();
+    paintCanvas(false);
     
     var nextX = snakeArray[0].x;
     var nextY = snakeArray[0].y;
@@ -132,6 +154,7 @@ function paint() {
 
     paintCell(food.x, food.y);
     var scoreText = "Score: " + score;
+    context.fillStyle = "black";
     context.fillText(scoreText, 5, canvasHeight-5);
 }
 
@@ -154,16 +177,19 @@ function checkCollision(x, y, array) {
 $(document).keydown(function(e) {
     var key = e.which;
 
-    if(key == "37" && direction != "right") {
+    if(key == "32" && !gameStarted && difficultySelected) {
+        init();
+    }
+    else if((key == "37" || key == "65") && direction != "right") {
         direction = "left";
     }
-    else if(key == "38" && direction != "down") {
+    else if((key == "38" || key == "87") && direction != "down") {
         direction = "up";
     }
-    else if(key == "39" && direction != "left") {
+    else if((key == "39" || key == "68") && direction != "left") {
         direction = "right";
     }
-    else if(key == "40" && direction != "up") {
+    else if((key == "40" || key == "83") && direction != "up") {
         direction = "down";
     }
 });
