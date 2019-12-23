ba77263dee2f78efb536167c718aeb211f974c81
Austin Kurtti <austin.kurtti@gmail.com>
2019-12-23 17:02:03 -0600

    Renamed angular project and removed top folder

--- src/app/core/app.component.spec.ts
+++ src/app/core/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
         expect(app).toBeTruthy();
     });
 
-    it(`should have as title 'kurtti-site'`, () => {
+    it(`should have as title 'personal'`, () => {
         const fixture = TestBed.createComponent(AppComponent);
         const app = fixture.debugElement.componentInstance;
-        expect(app.title).toEqual('kurtti-site');
+        expect(app.title).toEqual('personal');
     });
 
     it('should render title in a h1 tag', () => {
         const fixture = TestBed.createComponent(AppComponent);
         fixture.detectChanges();
         const compiled = fixture.debugElement.nativeElement;
-        expect(compiled.querySelector('h1').textContent).toContain('Welcome to kurtti-site!');
+        expect(compiled.querySelector('h1').textContent).toContain('Welcome to personal!');
     });
 });
