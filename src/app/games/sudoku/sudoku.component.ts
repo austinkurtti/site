9780e68da78f80c213a2f37479212e737d0e958d
Austin Kurtti <austin.kurtti@gmail.com>
2022-10-15 16:39:54 -0500

    Sudoku build logic and started adding actions

--- src/app/games/sudoku/sudoku.component.ts
+++ src/app/games/sudoku/sudoku.component.ts
@@ -1,35 +1,102 @@
-import { AfterViewInit, Component, ElementRef, OnInit, QueryList, Renderer2, ViewChild, ViewChildren } from '@angular/core';
-import { SudokuBoard, SudokuDifficulty } from './sudoku.models';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, QueryList, Renderer2, ViewChild, ViewChildren } from '@angular/core';
+import { BehaviorSubject, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { SudokuBoard, SudokuDifficulty, SudokuState } from './sudoku.models';
 
 @Component({
     selector: 'ak-sudoku',
     styleUrls: ['./sudoku.component.scss'],
     templateUrl: './sudoku.component.html'
 })
-export class SudokuComponent implements OnInit, AfterViewInit {
+export class SudokuComponent implements OnInit, AfterViewInit, OnDestroy {
     @ViewChild('sudokuContent') contentEl: ElementRef;
     @ViewChild('sudokuBoard') boardEl: ElementRef;
     @ViewChildren('sudokuCell') cellEls: QueryList<ElementRef>;
 
     public board = new SudokuBoard();
+    public difficulty = SudokuDifficulty.easy;
+
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    public SudokuDifficulty = SudokuDifficulty;
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    public SudokuState = SudokuState;
+
+    public building$ = new BehaviorSubject<boolean>(false);
+    public boardSize$ = new BehaviorSubject<number>(0);
+    public time$ = new BehaviorSubject<string>('00:00:00');
+
+    private _timerId;
+    private _startTime: number;
+    private _pauseTime: number;
+    private _pauseSum: number;
+
+    private _destroyed$ = new Subject<void>();
 
     constructor(
         private _renderer: Renderer2
     ) {}
 
     public ngOnInit(): void {
-        this.board.prime(SudokuDifficulty.easy);
+        this.boardSize$
+            .pipe(takeUntil(this._destroyed$))
+            .subscribe(size => {
+                if (size && this.boardEl) {
+                    this._renderer.setStyle(this.boardEl.nativeElement, 'height', `${size}px`);
+                    this._renderer.setStyle(this.boardEl.nativeElement, 'width', `${size}px`);
+                }
+            });
+
+        this.building$
+            .pipe(takeUntil(this._destroyed$))
+            .subscribe(building => {
+                if (!building) {
+                    this.board.state = SudokuState.running;
+                    this._startTime = Date.now();
+                    // this._timerId = setInterval(() => {
+                    //     const deltaSeconds = Math.floor((Date.now() - this._startTime) / 1000);
+                    //     const timeParts = [
+                    //         Math.floor(deltaSeconds / 3600),
+                    //         Math.floor((deltaSeconds % 3600) / 60),
+                    //         Math.floor(deltaSeconds % 60)
+                    //     ];
+                    //     this.time$.next(timeParts.join(':').replace(/\b(\d)\b/g, '0$1'));
+                    // }, 100);
+                    this._startTimer();
+                } else {
+                    this._pauseSum = 0;
+                    this.board.state = SudokuState.paused;
+                    this.time$.next('00:00:00');
+                    this._clearTimer();
+                }
+            });
+
+        this._buildSudoku();
     }
 
     public ngAfterViewInit(): void {
-        const contentHeight = this.contentEl.nativeElement.clientHeight;
-        const contentWidth = this.contentEl.nativeElement.clientWidth;
-        const size = contentWidth > contentHeight
-            ? contentHeight * 0.9 // Landscape viewport - constrain by height
-            : contentWidth * 0.9; // Portrait viewport - constrain by width
+        this._resizeBoard();
+    }
 
-        this._renderer.setStyle(this.boardEl.nativeElement, 'height', size + 'px');
-        this._renderer.setStyle(this.boardEl.nativeElement, 'width', size + 'px');
+    public ngOnDestroy(): void {
+        this._clearTimer();
+        this._destroyed$.next();
+    }
+
+    public changeState(): void {
+        if (this.board.state === SudokuState.paused) {
+            this.board.state = SudokuState.running;
+            this._pauseSum += Date.now() - this._pauseTime;
+            this._startTimer();
+        } else {
+            this._pauseTime = Date.now();
+            this.board.state = SudokuState.paused;
+            this._clearTimer();
+        }
+    }
+
+    public changeDifficulty(difficulty: SudokuDifficulty): void {
+        this.difficulty = difficulty;
+        this._buildSudoku();
     }
 
     public cellKeydown(rowIndex: number, colIndex: number, event: KeyboardEvent): void {
@@ -76,5 +143,42 @@ export class SudokuComponent implements OnInit, AfterViewInit {
         const nextRowIndex = rowIndex < 0 ? 0 : rowIndex > 8 ? 8 : rowIndex;
         const nextColIndex = colIndex < 0 ? 0 : colIndex > 8 ? 8 : colIndex;
         (document.querySelector(`#cell-${nextRowIndex}-${nextColIndex}`) as HTMLElement).focus();
+    }
+
+    private _buildSudoku(): void {
+        // TODO - Figure out why the Promise is blocking execution... using setTimeout to mimic expected behavior for now
+        this.building$.next(true);
+        setTimeout(() => {
+            this.board.build(this.difficulty).then(() => {
+                this.building$.next(false);
+            });
+        }, 0);
+    }
+
+    private _resizeBoard(): void {
+        const contentHeight = this.contentEl.nativeElement.clientHeight;
+        const contentWidth = this.contentEl.nativeElement.clientWidth;
+        const size = contentWidth > contentHeight
+            ? contentHeight * 0.9 // Landscape viewport - constrain by height
+            : contentWidth * 0.9; // Portrait viewport - constrain by width
+        this.boardSize$.next(size);
+    }
+
+    private _startTimer(): void {
+        this._timerId = setInterval(() => {
+            const deltaSeconds = Math.floor((Date.now() - (this._startTime + this._pauseSum)) / 1000);
+            const timeParts = [
+                Math.floor(deltaSeconds / 3600),
+                Math.floor((deltaSeconds % 3600) / 60),
+                Math.floor(deltaSeconds % 60)
+            ];
+            this.time$.next(timeParts.join(':').replace(/\b(\d)\b/g, '0$1'));
+        }, 100);
+    }
+
+    private _clearTimer(): void {
+        if (this._timerId) {
+            clearInterval(this._timerId);
+        }
     }
 }
--- src/app/games/sudoku/sudoku.models.ts
+++ src/app/games/sudoku/sudoku.models.ts
@@ -1,9 +1,13 @@
-import { BehaviorSubject } from 'rxjs';
 
 export enum SudokuDifficulty {
     easy = 1,
-    medium = 2,
-    hard = 3
+    medium = 5,
+    hard = 15
+}
+
+export enum SudokuState {
+    paused = 1,
+    running = 2
 }
 
 export class SudokuCell {
@@ -13,11 +17,37 @@ export class SudokuCell {
 }
 
 export class SudokuBoard {
+    public state = SudokuState.paused;
     public cells: Array<Array<SudokuCell>> = [];
 
-    public primed$ = new BehaviorSubject<boolean>(false);
-
     private _possibleValues = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    private _possibleSolutions = 0;
+
+    public get valid(): boolean {
+        for (let i = 0; i < 9; i++) {
+            const rowSum = this.cells[i].map(cell => cell.value).reduce((sum, cur) => sum + cur);
+            if (rowSum !== 45) {
+                return false;
+            }
+
+            const colSum = this.cells.map(row => row[i].value).reduce((sum, cur) => sum + cur);
+            if (colSum !== 45) {
+                return false;
+            }
+
+            const squareRowStart = Math.floor(i / 3) * 3;
+            const squareRowEnd = squareRowStart + 3;
+            const squareColStart = i % 3;
+            const squareColEnd = squareColStart + 3;
+            const square = this.cells.slice(squareRowStart, squareRowEnd).map(squareRow => squareRow.slice(squareColStart, squareColEnd));
+            const squareValues = [...square[0], ...square[1], ...square[2]].map(squareCell => squareCell.value);
+            const squareSum = squareValues.reduce((sum, cur) => sum + cur);
+            if (squareSum !== 45) {
+                return false;
+            }
+        }
+        return true;
+    }
 
     private get _shuffledValues(): number[] {
         const shuffledVals = [...this._possibleValues];
@@ -28,40 +58,35 @@ export class SudokuBoard {
         return shuffledVals;
     }
 
-    private get _isFull(): boolean {
-        for (const row of this.cells) {
-            for (const cell of row) {
-                if (cell.value === null) {
-                    return false;
-                }
-            };
-        };
-        return true;
-    }
-
-    public prime(difficulty: SudokuDifficulty): void {
-        this.primed$.next(false);
+    public async build(difficulty: SudokuDifficulty): Promise<void> {
         this.cells = [...new Array(9)].map(() => [...new Array(9)].map(() => new SudokuCell()));
 
+        // First build a new valid solution
         this._fillBoard();
 
-        this.primed$.next(true);
+        // Remove enough values according to the desired difficulty
+        await this._makePuzzle(difficulty);
     }
 
     private _fillBoard(): boolean {
+        // Find the next blank cell
         let row: number;
         let col: number;
         for (let i = 0; i < 81; i++) {
             row = Math.floor(i / 9);
             col = i % 9;
             if (this.cells[row][col].value === null) {
+                // Shuffle the possible values to keep randomizing the solution generation
                 const shuffledValues = this._shuffledValues;
                 for (const value of shuffledValues) {
-                    if (this._rowValid(row, value)) {
-                        if (this._colValid(col, value)) {
-                            if (this._squareValid(row, col, value)) {
+                    // Make sure the row, column and square are all valid
+                    if (this._rowValid(this.cells, row, value)) {
+                        if (this._colValid(this.cells, col, value)) {
+                            if (this._squareValid(this.cells, row, col, value)) {
+                                // Mark this cell with the candidate value
                                 this.cells[row][col].value = value;
-                                if (this._isFull) {
+                                if (this._isFull(this.cells)) {
+                                    // The board is complete
                                     return true;
                                 } else if (this._fillBoard()) {
                                     return true;
@@ -76,22 +101,110 @@ export class SudokuBoard {
         this.cells[row][col].value = null;
     }
 
-    private _rowValid(row: number, value?: number): boolean {
-        const rowValues = this.cells[row].map(cell => cell.value);
+    private _makePuzzle(difficulty: SudokuDifficulty): Promise<void> {
+        return new Promise<void>(resolve => {
+            let attempts = difficulty as number; // make this related to difficulty
+
+            while (attempts > 0) {
+                // Pick a random cell to blank out
+                let row = Math.floor(Math.random() * 9);
+                let col = Math.floor(Math.random() * 9);
+                while (this.cells[row][col].value === null) {
+                    row = Math.floor(Math.random() * 9);
+                    col = Math.floor(Math.random() * 9);
+                }
+
+                // Remember the cell value in case in needs to be restored
+                const originalValue = this.cells[row][col].value;
+                this.cells[row][col].value = null;
+
+                // Copy the board so the solver can modify it to find solutions wihtout affecting the master
+                const copy = [...new Array(9)].map(() => [...new Array(9)].map(() => new SudokuCell()));
+                copy.forEach((copyRow, rowIndex) => {
+                    copyRow.forEach((copyCell, cellIndex) => {
+                        copyCell.value = this.cells[rowIndex][cellIndex].value;
+                    });
+                });
+
+                // Reset solution counter and find how many solutions now exist
+                this._possibleSolutions = 0;
+                this._solveBoard(copy);
+
+                if (this._possibleSolutions !== 1) {
+                    // Cancel the change to the board because there is an invalid number of solutions
+                    this.cells[row][col].value = originalValue;
+                    attempts--;
+                }
+            }
+
+            this.cells.forEach(row => {
+                row.forEach(cell => {
+                    cell.given = cell.value !== null;
+                });
+            });
+
+            resolve();
+        });
+    }
+
+    private _solveBoard(cells: Array<Array<SudokuCell>>): boolean {
+        // Find the next blank cell
+        let row: number;
+        let col: number;
+        for (let i = 0; i < 81; i++) {
+            row = Math.floor(i / 9);
+            col = i % 9;
+            if (cells[row][col].value === null) {
+                // Find the next value that doesn't break the solution, if any
+                for (const value of this._possibleValues) {
+                    if (this._rowValid(cells, row, value)) {
+                        if (this._colValid(cells, col, value)) {
+                            if (this._squareValid(cells, row, col, value)) {
+                                cells[row][col].value = value;
+                                if (this._isFull(cells)) {
+                                    // This is a possible solution, but there could be more
+                                    this._possibleSolutions++;
+                                    break;
+                                } else if (this._solveBoard(cells)) {
+                                    return true;
+                                }
+                            }
+                        }
+                    }
+                }
+                break;
+            }
+        }
+        cells[row][col].value = null;
+    }
+
+    private _isFull(cells: Array<Array<SudokuCell>>): boolean {
+        for (const row of cells) {
+            for (const cell of row) {
+                if (cell.value === null) {
+                    return false;
+                }
+            };
+        };
+        return true;
+    }
+
+    private _rowValid(cells: Array<Array<SudokuCell>>, row: number, value?: number): boolean {
+        const rowValues = cells[row].map(cell => cell.value);
         return !rowValues.includes(value);
     }
 
-    private _colValid(col: number, value?: number): boolean {
-        const colValues = this.cells.map(row => row[col].value);
+    private _colValid(cells: Array<Array<SudokuCell>>, col: number, value?: number): boolean {
+        const colValues = cells.map(row => row[col].value);
         return !colValues.includes(value);
     }
 
-    private _squareValid(row: number, col: number, value?: number): boolean {
+    private _squareValid(cells: Array<Array<SudokuCell>>, row: number, col: number, value?: number): boolean {
         const squareRowStart = Math.floor(row / 3) * 3;
         const squareRowEnd = squareRowStart + 3;
         const squareColStart = Math.floor(col / 3) * 3;
         const squareColEnd = squareColStart + 3;
-        const square = this.cells.slice(squareRowStart, squareRowEnd).map(squareRow => squareRow.slice(squareColStart, squareColEnd));
+        const square = cells.slice(squareRowStart, squareRowEnd).map(squareRow => squareRow.slice(squareColStart, squareColEnd));
         const squareValues = [...square[0], ...square[1], ...square[2]].map(squareCell => squareCell.value);
         return !squareValues.includes(value);
     }
