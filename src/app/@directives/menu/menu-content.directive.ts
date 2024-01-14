f226246e6be56f1568f733a16c5ee0bdcc32fb17
Austin Kurtti <austin.kurtti@gmail.com>
2024-01-14 14:00:31 -0600

    Better focus support for menus

--- src/app/@directives/menu/menu-content.directive.ts
+++ src/app/@directives/menu/menu-content.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, HostBinding, OnDestroy, OnInit, inject } from '@angular/core';
+import { Directive, ElementRef, HostBinding, OnDestroy, OnInit, inject } from '@angular/core';
 import { MenuDirective } from './menu.directive';
 
 @Directive({
@@ -9,16 +9,32 @@ export class MenuContentDirective implements OnInit, OnDestroy {
 
     public menu = inject(MenuDirective);
 
+    private _elementRef = inject(ElementRef);
+
     public ngOnInit(): void {
-        document.addEventListener('click', this._closeMenuEventListener);
+        document.addEventListener('click', this._documentClickListener);
+        this._elementRef.nativeElement.addEventListener('focusin', this._focusInListener);
     }
 
     public ngOnDestroy(): void {
-        document.removeEventListener('click', this._closeMenuEventListener);
+        document.removeEventListener('click', this._documentClickListener);
+        this._elementRef.nativeElement.removeEventListener('focusin', this._focusInListener);
+        this._elementRef.nativeElement.removeEventListener('focusout', this._focusOutListener);
     }
 
-    private _closeMenuEventListener = (event: PointerEvent): void => {
-        // If this click event happens at all, then a click has occurred outside the menu
+    private _documentClickListener = (event: PointerEvent): void => {
+        // Any click after a menu is opened should result in the menu closing
         this.menu.close();
+    };
+
+    private _focusInListener = (event: FocusEvent): void => {
+        this._elementRef.nativeElement.addEventListener('focusout', this._focusOutListener);
+        this._elementRef.nativeElement.removeEventListener('focusin', this._focusInListener);
+    };
+
+    private _focusOutListener = (event: FocusEvent): void => {
+        if (event.relatedTarget && !(event.currentTarget as HTMLElement).contains(event.relatedTarget as HTMLElement)) {
+            this.menu.close();
+        }
     };
 }
