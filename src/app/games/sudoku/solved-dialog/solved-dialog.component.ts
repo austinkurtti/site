9059a85d9e24ca4f93cfeff9bd562a52ea9a4b55
Austin Kurtti <austin.kurtti@gmail.com>
2024-01-19 00:29:39 -0600

    #53 Fixed incorrectly formatted time on solved dialog

--- src/app/games/sudoku/solved-dialog/solved-dialog.component.ts
+++ src/app/games/sudoku/solved-dialog/solved-dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { DialogBase } from '@directives/dialog/dialog-base';
 import { DifficultyPipeModule } from '@pipes/difficulty/difficulty.module';
 import { SudokuDifficulty } from '../sudoku.models';
@@ -12,31 +12,9 @@ import { SudokuDifficulty } from '../sudoku.models';
     styleUrls: ['./solved-dialog.component.scss'],
     templateUrl: './solved-dialog.component.html'
 })
-export class SolvedDialogComponent extends DialogBase implements OnInit {
+export class SolvedDialogComponent extends DialogBase {
     @Input() difficulty: SudokuDifficulty;
     @Input() time: string;
     @Input() goHome: () => void;
     @Input() playAgain: () => void;
-
-    // eslint-disable-next-line @typescript-eslint/naming-convention
-    public SudokuDifficulty = SudokuDifficulty;
-
-    public timeValue = 'PT';
-    public timeDisplay = '';
-
-    public ngOnInit(): void {
-        const timeParts = this.time.replace(/0/g, '').split(':');
-        if (timeParts[0]) {
-            this.timeValue += `${timeParts[0]}H`;
-            this.timeDisplay += `${timeParts[0]}h`;
-        }
-        if (timeParts[1]) {
-            this.timeValue += `${timeParts[1]}M`;
-            this.timeDisplay += ((this.timeDisplay ? ' ' : '') + `${timeParts[1]}m`);
-        }
-        if (timeParts[2]) {
-            this.timeValue += `${timeParts[2]}S`;
-            this.timeDisplay += ((this.timeDisplay ? ' ' : '') + `${timeParts[2]}s`);
-        }
-    }
 }
