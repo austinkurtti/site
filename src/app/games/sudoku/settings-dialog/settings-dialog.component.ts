5e9156f76dd839a1164653c363a49e377ee6b726
Austin Kurtti <austin.kurtti@gmail.com>
2024-06-10 03:46:58 -0500

    #118 Finished settings dialog facelift
    Was covered partly by commit for #56. This commit gives groups a distinct background and collapses descriptions into tooltip icons for smaller screens.

--- src/app/games/sudoku/settings-dialog/settings-dialog.component.ts
+++ src/app/games/sudoku/settings-dialog/settings-dialog.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { ToggleComponent } from '@components/toggle/toggle.component';
 import { DialogBase } from '@directives/dialog/dialog-base';
+import { TooltipDirective } from '@directives/tooltip/tooltip.directive';
 import { LocalStorageService } from '@services/local-storage.service';
 import { SudokuManager } from '../sudoku-manager';
 
@@ -9,7 +10,8 @@ import { SudokuManager } from '../sudoku-manager';
     standalone: true,
     imports: [
         CommonModule,
-        ToggleComponent
+        ToggleComponent,
+        TooltipDirective
     ],
     selector: 'ak-settings-dialog',
     styleUrls: ['./settings-dialog.component.scss'],
@@ -17,6 +19,10 @@ import { SudokuManager } from '../sudoku-manager';
 })
 export class SettingsDialogComponent extends DialogBase {
     public gameManager = inject(SudokuManager);
+
+    public erasePencilDescription = 'After entering a number, any conflicting penciled in values in the same row, column and 3x3 square will be erased';
+    public disableInputsDescription = 'Number inputs will be disabled when the count for their values reaches nine';
+    public exitSaveDescription = 'Sudoku progress will be automatically saved when exiting the game';
 
     public toggleChange(setting: string, value: boolean): void {
         LocalStorageService.setItem(`${this.gameManager.localStoragePrefix}_${setting}`, value);
