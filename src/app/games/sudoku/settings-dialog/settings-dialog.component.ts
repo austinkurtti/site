1d736120ef00e3751aac9022227e39174fb60c11
Austin Kurtti <austin.kurtti@gmail.com>
2025-01-16 17:07:27 -0600

    Sass migration from @import to @use
    Also removed unused TooltipDirective

--- src/app/games/sudoku/settings-dialog/settings-dialog.component.ts
+++ src/app/games/sudoku/settings-dialog/settings-dialog.component.ts
@@ -2,15 +2,13 @@ import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { ToggleComponent } from '@components/toggle/toggle.component';
 import { DialogBaseDirective } from '@directives/dialog/dialog-base.directive';
-import { TooltipDirective } from '@directives/tooltip/tooltip.directive';
 import { LocalStorageService } from '@services/local-storage.service';
 import { SudokuManager } from '../sudoku-manager';
 
 @Component({
     imports: [
         CommonModule,
-        ToggleComponent,
-        TooltipDirective
+        ToggleComponent
     ],
     selector: 'ak-settings-dialog',
     styleUrls: ['./settings-dialog.component.scss'],
