daa35c540e4ba8ecdd028f911dd0ac6542062d14
Austin Kurtti <austin.kurtti@gmail.com>
2025-01-21 00:52:21 -0600

    Project section content
    - Plus a couple icon hovering style fixes

--- src/app/home/projects/project/project.component.ts
+++ src/app/home/projects/project/project.component.ts
@@ -1,10 +1,21 @@
-import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, computed, input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'ak-project',
     styleUrls: ['./project.component.scss'],
-    templateUrl: './project.component.html'
+    templateUrl: './project.component.html',
+    imports: [
+        CommonModule,
+        RouterLink
+    ]
 })
 export class ProjectComponent {
-    @Input() name: string;
+    public name = input.required<string>();
+    public icon = input.required<string>();
+    public gitHubLink = input<string>();
+    public externalLink = input<string>();
+
+    public hasLinks = computed(() => this.gitHubLink() || this.externalLink());
 }
