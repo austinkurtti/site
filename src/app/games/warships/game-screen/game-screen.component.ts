dcd565e30a699b2e4d35f98f964f30386f96353a
Austin Kurtti <austin.kurtti@gmail.com>
2025-10-22 14:28:13 -0500

    #154 Warships grid and drag/drop progress
    - Built basic grid structure that fills available space while staying square
    - Added grid labels that align with rows and columns neatly
    - Started working on basic drag/drop functionality
    - Added ship model and related enums

--- src/app/games/warships/game-screen/game-screen.component.ts
+++ src/app/games/warships/game-screen/game-screen.component.ts
@@ -1,14 +1,77 @@
-import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, signal } from '@angular/core';
 import { WarshipsManager } from '../warships-manager';
-import { WarshipsScreenState } from '../warships.models';
+import { WarshipsGameState, WarshipsScreenState, WarshipsShip } from '../warships.models';
 
 @Component({
     selector: 'ak-warships-game-screen',
     styleUrls: ['./game-screen.component.scss'],
-    templateUrl: './game-screen.component.html'
+    templateUrl: './game-screen.component.html',
+    imports: [
+        CommonModule
+    ]
 })
 export class WarshipsGameScreenComponent {
     public gameManager = inject(WarshipsManager);
+
+    public gameState = signal(WarshipsGameState.deploying);
+    public deployableShips = [
+        new WarshipsShip('Carrier', 5),
+        new WarshipsShip('Battleship', 4),
+        new WarshipsShip('Destroyer', 3),
+        new WarshipsShip('Submarine', 3),
+        new WarshipsShip('Patrol Ship', 2)
+    ];
+
+    public GameState = WarshipsGameState;
+    public String = String;
+
+    public shipDragStart = (event: DragEvent) => {
+        const ship = event.currentTarget as HTMLElement;
+        ship.id = 'dragged-ship';
+        event.dataTransfer.effectAllowed = 'move';
+        event.dataTransfer.setData('ship', ship.getAttribute('data-ship'));
+    }
+
+    public shipDragEnd = (event: DragEvent) => {
+        const ship = event.currentTarget as HTMLElement;
+        ship.removeAttribute('id');
+    }
+
+    public sectorDragOver = (event: DragEvent) => {
+        if (event.dataTransfer.types.includes('ship')) {
+            const sector = event.currentTarget as HTMLElement;
+            const draggedShip = document.getElementById('dragged-ship');
+            const existingShip = sector.querySelector('.deployable-ship');
+
+            if (!existingShip) {
+                event.preventDefault();
+
+                const existingPlaceholder = sector.querySelector('.deployable-ship-placeholder');
+                if (!existingPlaceholder) {
+                    sector.appendChild(this._createShipPlaceholder(draggedShip));
+                }
+            }
+        }
+    }
+
+    public sectorDragLeave = (event: DragEvent) => {
+        const sector = event.currentTarget as HTMLElement;
+        const placeholder = sector.querySelector('.deployable-ship-placeholder');
+        placeholder?.remove();
+    }
+
+    public sectorDrop = (event: DragEvent) => {
+        event.preventDefault();
+
+        const sector = event.currentTarget as HTMLElement;
+        const placeholder = sector.querySelector('.deployable-ship-placeholder');
+        placeholder?.remove();
+
+        const draggedShip = document.getElementById('dragged-ship');
+        draggedShip.remove();
+        sector.appendChild(draggedShip);
+    }
 
     public showSettingsDialog(): void {
         // TODO
@@ -17,5 +80,12 @@ export class WarshipsGameScreenComponent {
     public quit(): void {
         this.gameManager.gameInstance = null;
         this.gameManager.screen.set(WarshipsScreenState.menu);
+    }
+
+    private _createShipPlaceholder(draggedShip: HTMLElement) {
+        const placeholder = document.createElement('div');
+        placeholder.classList.add('deployable-ship-placeholder');
+        placeholder.setAttribute(draggedShip.getAttributeNames().filter(name => name.startsWith('_ngcontent'))[0], '');
+        return placeholder;
     }
 }
--- src/app/games/warships/warships.models.ts
+++ src/app/games/warships/warships.models.ts
@@ -3,6 +3,14 @@ export enum WarshipsScreenState {
     game = 2
 }
 
+export enum WarshipsGameState {
+    deploying = 1,
+    running = 2,
+    paused = 3,
+    won = 4,
+    lost = 5
+}
+
 export enum WarshipsSectorState {
     empty   = 1 << 0,
     ship    = 1 << 1,
@@ -21,4 +29,22 @@ export class WarshipsGrid {
 export class WarshipsGameInstance {
     public playerGrid: WarshipsGrid;
     public computerGrid: WarshipsGrid;
+}
+
+export enum WarshipsShipOrientation {
+    horizontal = 1,
+    vertical = 2
+}
+
+export class WarshipsShip {
+    public id: string;
+    public name: string;
+    public orientation = WarshipsShipOrientation.horizontal;
+    public length: number;
+
+    constructor(name: string, length: number) {
+        this.id = name.replaceAll(' ', '-').toLowerCase();
+        this.name = name;
+        this.length = length;
+    }
 }
