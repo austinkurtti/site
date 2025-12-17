bbc7ac30d84f90ec0d150cf48c9a0c1fea83da4b
Austin Kurtti <austin.kurtti@gmail.com>
2025-12-17 16:44:40 -0600

    Added header back to games home page

--- src/app/games/games-home.component.ts
+++ src/app/games/games-home.component.ts
@@ -1,6 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { ThemeComponent } from '@components/theme/theme.component';
+import { MenuContentDirective } from '@directives/menu/menu-content.directive';
+import { MenuDirective, MenuPosition } from '@directives/menu/menu.directive';
 import { GamesService } from './games.service';
 
 @Component({
@@ -9,9 +12,14 @@ import { GamesService } from './games.service';
     templateUrl: './games-home.component.html',
     imports: [
         CommonModule,
-        RouterLink
+        MenuDirective,
+        MenuContentDirective,
+        RouterLink,
+        ThemeComponent
     ]
 })
 export class GamesHomeComponent {
     public gamesService = inject(GamesService);
+
+    public MenuPosition = MenuPosition;
 }
