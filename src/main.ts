140fe4fad866c1323e8129ca59d82916d4ad2463
Austin Kurtti <austin.kurtti@gmail.com>
2026-04-27 15:45:55 -0500

    Zoneless
    Plus fixed a few TS errors

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { enableProdMode, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { AppComponent } from './app/core/app.component';
@@ -15,8 +15,7 @@ bootstrapApplication(
     {
         providers: [
             importProvidersFrom(BrowserModule, CommonModule),
-            provideRouter(appRoutes),
-            provideZoneChangeDetection()
+            provideRouter(appRoutes)
         ]
     }
 ).catch(err => console.error(err));
